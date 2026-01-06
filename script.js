a0c83959ea6146243cfa924e69cf3acd51d35c40
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-06 20:52:39 +0200

    Edit js to make success text show only when inputs are valid and animate the process

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const submit_text = document.querySelector('.after_submit');
-const btn = document.querySelector('.cta_btn');
+const successMessage = document.querySelector('.after_submit');
+const form = document.querySelector('#unfoldForm');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.style.display = 'none';
+    successMessage.style.display = 'block';
+    successMessage.classList.add('fade-in');
+})
 
 btn.addEventListener('click', () => {
     submit_text.setAttribute('style', 'display: block');
